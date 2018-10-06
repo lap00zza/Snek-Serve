9444cba8a788431dc63dcbaa2daf9c6f7e16f450
Jewel Mahanta <jewelmahanta@gmail.com>
2018-10-06 13:40:11 +0530

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# `Snek Serve`
+SnekServe is a very light file server. Features:
+* adds `Access-Control-Allow-Origin` header on all responses
+* directory listing
+
+## Usage
+```
+# 8080 or any other port
+snekserver 8080
+```
+
+## License
+MIT
---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+
+const PORT = process.argv[2] || 8080;
+const _access = util.promisify(fs.access);
+const _readdir = util.promisify(fs.readdir);
+
+const genDirHTML = (files) => {
+    let d_listing = "<pre>\n";
+    files.forEach(file => {
+        file.isDir
+            ? d_listing += `<a href="${file.name}/">${file.name}/</a>\n`
+            : d_listing += `<a href="${file.name}">${file.name}</a>\n`;
+    });
+    return d_listing + "</pre>";
+};
+
+const server = http.createServer(async (req, res) => {
+    console.log("::request::", req.url);
+    const fp = path.join(process.cwd(), req.url);
+    try {
+        // if a file does not exist we send a 404 from catch
+        await _access(fp, fs.constants.F_OK);
+        if (fs.statSync(fp).isDirectory()) {
+            let files = await _readdir(fp);
+            files = files.map(file => {
+                let isDir = false;
+                try {
+                    isDir = fs.statSync(path.join(fp, file)).isDirectory();
+                } catch (e) {
+                    console.error(e);
+                }
+                return {
+                    name: file,
+                    isDir
+                };
+            });
+            res.writeHead(200, {
+                "Content-Type": "text/html",
+                "Access-Control-Allow-Origin": "*"
+            });
+            res.end(genDirHTML(files));
+        } else {
+            res.writeHead(200, {
+                "Content-Length": fs.statSync(fp).size,
+                "Access-Control-Allow-Origin": "*"
+            });
+            fs.createReadStream(fp).pipe(res);
+        }
+    } catch (e) {
+        console.error(e);
+        res.writeHead(404);
+        res.end(":(");
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Serving on http://localhost:${PORT}`);
+});
